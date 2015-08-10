875434aa14653b0469508665652ffc683613d29a
WangMinjun <15895899337@163.com>
2015-08-10 15:49:37 +0800

    Initial Commit

---
+++ MJStock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJStock
+//
+//  Created by WangMinjun on 15/8/10.
+//  Copyright (c) 2015年 WangMinjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJStock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJStock
+//
+//  Created by WangMinjun on 15/8/10.
+//  Copyright (c) 2015年 WangMinjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
